85373a55fe551c46a4ef1e8a54bf2fd746e9b0c8
kennaas <33057681+kennaas@users.noreply.github.com>
2020-12-16 13:49:56 +0100

    Update loter_result_conversion.R

--- loter_result_conversion.R
+++ loter_result_conversion.R
@@ -2,62 +2,84 @@ library(data.table)
 library(BGData)
 source("My_R_code/file_backed_mat.R")
 
-innerFile = file("Data/loter/Run 3/Loter input 3/inner_phased3.vcf.gz",open = "r")
+loter_run = 4
+
+innerFile = file(paste0("Data/loter/Run ", loter_run,
+                        "/Loter input ", loter_run,
+                        "/inner_phased", loter_run, ".vcf.gz"), 
+                 open = "r")
 innerLine  = scan(innerFile, nlines = 1, skip = 8,
                   what = character(), quiet = TRUE)
 close(innerFile)
 innerInds = innerLine[-(1:9)]
 
-outerFile = file("Data/loter/Run 3/Loter input 3/outer_phased3.vcf.gz",open = "r")
+outerFile = file(paste0("Data/loter/Run ", loter_run,
+                        "/Loter input ", loter_run,
+                        "/outer_phased", loter_run, ".vcf.gz"), 
+                 open = "r")
 outerLine  = scan(outerFile, nlines = 1, skip = 8,
                   what = character(), quiet = TRUE)
 close(outerFile)
 outerInds = outerLine[-(1:9)]
 
-otherFile = file("Data/loter/Run 3/Loter input 3/other_phased3.vcf.gz",open = "r")
+otherFile = file(paste0("Data/loter/Run ", loter_run,
+                        "/Loter input ", loter_run,
+                        "/other_phased", loter_run, ".vcf.gz"), 
+                 open = "r")
 otherLine  = scan(otherFile, nlines = 1, skip = 8,
                   what = character(), quiet = TRUE)
 close(otherFile)
 otherInds = otherLine[-(1:9)]
 
-admixedFile = file("Data/loter/Run 3/Loter input 3/admixed_phased3.vcf.gz",open = "r")
+admixedFile = file(paste0("Data/loter/Run ", loter_run,
+                          "/Loter input ", loter_run,
+                          "/admixed_phased", loter_run, ".vcf.gz"), 
+                   open = "r")
 admixedLine  = scan(admixedFile, nlines = 1, skip = 8,
                   what = character(), quiet = TRUE)
 close(admixedFile)
 admixedInds = admixedLine[-(1:9)]
-SNPs = fread(file = "Data/loter/Run 3/Loter input 3/admixed_phased3.vcf.gz",
+SNPs = fread(file = paste0("Data/loter/Run ", loter_run,
+                           "/Loter input ", loter_run,
+                           "/admixed_phased", loter_run,".vcf.gz"),
               select = 3)$ID
 
 
 inds = c(innerInds, outerInds, otherInds, admixedInds)
 
-AOuter = BGData(geno =
-                 initFileBackedMatrix(length(inds), 2 * length(SNPs),
-                                       folderOut = "Data/loter/Run 3/AOuter",
-                                       outputType = "boolean"),
-                pheno = data.frame(ID = inds))
-AInner = BGData(geno =
-                 initFileBackedMatrix(length(inds), 2 * length(SNPs),
-                                       folderOut = "Data/loter/Run 3/AInner",
-                                       outputType = "boolean"),
-                pheno = data.frame(ID = inds))
-AOther = BGData(geno =
-                initFileBackedMatrix(length(inds), 2 * length(SNPs),
-                                       folderOut = "Data/loter/Run 3/AOther",
-                                       outputType = "boolean"),
-                pheno = data.frame(ID = inds))
+AOuter = BGData(
+  geno = initFileBackedMatrix(length(inds), 2 * length(SNPs),
+                              folderOut = paste0("Data/loter/Run ",
+                                                 loter_run,
+                                                 "/AOuter"),
+                              outputType = "boolean"),
+  pheno = data.frame(ID = inds))
+AInner = BGData(
+  geno = initFileBackedMatrix(length(inds), 2 * length(SNPs),
+                              folderOut = paste0("Data/loter/Run ",
+                                                 loter_run,
+                                                 "/AInner"),
+                              outputType = "boolean"),
+  pheno = data.frame(ID = inds))
+AOther = BGData(
+  geno = initFileBackedMatrix(length(inds), 2 * length(SNPs),
+                              folderOut = paste0("Data/loter/Run ",
+                                                 loter_run,
+                                                 "/AOther"),
+                              outputType = "boolean"),
+  pheno = data.frame(ID = inds))
 
 i = c(1, length(innerInds))
 for (row in i[1]:i[2]) {
-  AOuter@geno[row, ] = AOther@geno[row, ] = rep(0, 2 * length(SNPs))
-  AInner@geno[row, ] = rep(1, 2 * length(SNPs))
+  AOuter@geno[row,] = AOther@geno[row,] = rep(0, 2 * length(SNPs))
+  AInner@geno[row,] = rep(1, 2 * length(SNPs))
   print(row)
 }
 
 i = i + c(length(innerInds), length(outerInds))
 
 for (row in i[1]:i[2]) {
-  AInner@geno[row, ] = AOther@geno[row, ] = rep(0, 2 * length(SNPs))
+  AInner@geno[row, ] = AOther@geno[row,] = rep(0, 2 * length(SNPs))
   AOuter@geno[row, ] = rep(1, 2 * length(SNPs))
   print(row)
 }
@@ -65,7 +87,7 @@ for (row in i[1]:i[2]) {
 i = i + c(length(outerInds), length(otherInds))
 
 for (row in i[1]:i[2]) {
-  AInner@geno[row, ] = AOuter@geno[row, ] = rep(0, 2 * length(SNPs))
+  AInner@geno[row, ] = AOuter@geno[row,] = rep(0, 2 * length(SNPs))
   AOther@geno[row, ] = rep(1, 2 * length(SNPs))
   print(row)
 }
@@ -73,7 +95,9 @@ i = i + c(length(otherInds), length(admixedInds))
 # Order in loter: Outer == 0, Inner == 1, Other == 2
 SNPs1 = seq(1, by = 2, to = 2 * length(SNPs))
 SNPs2 = seq(2, by = 2, to = 2 * length(SNPs))
-admixedFile = file("Data/loter/Run 3/loter_result3.txt",open = "r")
+admixedFile = file(paste0("Data/loter/Run ", loter_run,
+                          "/loter_result", loter_run, ".txt"), 
+                   open = "r")
 for (row in i[1]:i[2]) {
   line1  = scan(admixedFile, nlines = 1, 
                 what = integer(), quiet = TRUE)
@@ -89,16 +113,20 @@ for (row in i[1]:i[2]) {
 }
 close(admixedFile)
 
-load.BGData(file = "Data/loter/Run 3/W/W.RData")
+load.BGData(file = paste0("Data/loter/Run ", loter_run,
+                          "/W/W.RData"))
 
 dimnames(AInner@geno) = dimnames(W@geno)
 dimnames(AOuter@geno) = dimnames(W@geno)
 dimnames(AOther@geno) = dimnames(W@geno)
 
 
-save(AInner, file = "Data/loter/Run 3/AInner/AInner.RData")
-save(AOuter, file = "Data/loter/Run 3/AOuter/AOuter.RData")
-save(AOther, file = "Data/loter/Run 3/AOther/AOther.RData")
+save(AInner, file = paste0("Data/loter/Run ", loter_run,
+                           "/AInner/AInner.RData"))
+save(AOuter, file = paste0("Data/loter/Run ", loter_run,
+                           "/AOuter/AOuter.RData"))
+save(AOther, file = paste0("Data/loter/Run ", loter_run,
+                           "/AOther/AOther.RData"))
 
 
 # write.table(
