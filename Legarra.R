bdf4563b75970217df76104391d963c493f96942
kennaas <33057681+kennaas@users.noreply.github.com>
2021-01-04 16:41:52 +0100

    Documentation

--- Legarra.R
+++ Legarra.R
@@ -1,9 +1,13 @@
 library(BGData)
 library(INLA)
 
+# Pick response and local ancestry run
+response = "wing"
 loter_run = 3
-cores = ifelse(Sys.info()[['sysname']] == "Windows", 1, 8)
 
+################### Load data #####################################
+
+# load GRMs
 load(paste0("Runs/GRMs/GRM_rio", loter_run, "_Inner.RData"))
 innerGRM = G_VR
 load(paste0("Runs/GRMs/GRM_rio", loter_run, "_Outer.RData"))
@@ -12,30 +16,30 @@ load(paste0("Runs/GRMs/GRM_rio", loter_run, "_Other.RData"))
 otherGRM = G_VR
 rm(G_VR, G_GCTA, pi, delta)
 
-response = "wing"
-
+# Load phenotypic data
 load("Runs/morphData_pedigree_version.RData")
 
+# Load individuals in each ref or admixed populations
 load(paste0("Data/Founders", loter_run, "/founders.RData"))
-
 innerInds = na.omit(unique(innerInds))
 outerInds = na.omit(unique(outerInds))
 otherInds = na.omit(unique(otherInds))
 admixedInds = admixedInds[admixedInds %in% morphData$ringnr]
 
+# Load INLA results
 load(paste0("Runs/", response, "/inla_result_hetGG_rio3_VR_", response, ".RData"))
 modelRio = model
 load(paste0("Runs/", response, "/inla_result_hetGG_pedigree_", response, ".RData"))
 modelPed = model
 rm(model)
-######################### Legarra scaling #######################
-
 #Load partial relatedness matrices
 load("Runs/A_hetped.RData")
 
-innerBasePop = c(innerInds)#, admixedInds)
-outerBasePop = c(outerInds)#, admixedInds)
-otherBasePop = c(otherInds)#, admixedInds)
+######################### Legarra scaling #######################
+
+innerBasePop = c(innerInds, admixedInds)
+outerBasePop = c(outerInds, admixedInds)
+otherBasePop = c(otherInds, admixedInds)
 
 innerGRM = innerGRM[colnames(innerGRM) %in% innerBasePop,
                     colnames(innerGRM) %in% innerBasePop]
@@ -84,35 +88,3 @@ save(innerRioLegarrad, innerPedLegarrad,
      outerRioLegarrad, outerPedLegarrad,
      otherRioLegarrad, otherPedLegarrad,
      file = paste0("Runs/legarra/", response, loter_run, ".RData"))
-
-######################## One time: find gamma matrices ############
-
-# load.BGData(file = paste0("Data/loter/Run ", loter_run, 
-#                           "/AInner/AInner.RData"))
-# 
-# load.BGData(file = paste0("Data/loter/Run ", loter_run, 
-#                           "/AOuter/AOuter.RData"))
-# 
-# load.BGData(file = paste0("Data/loter/Run ", loter_run, 
-#                           "/AOther/AOther.RData"))
-
-# thetaInner = getG(AInner@geno, 
-#              center = FALSE, scale = FALSE, scaleG = FALSE,
-#              nCores = cores, verbose = TRUE) 
-# thetaInner = thetaInner / dim(geno(AInner))[2]
-# gammaInner = innerGRM / thetaInner
-# save(gammaInner, file = "Runs/gammaInner.Rdata")
-# 
-# thetaOuter = getG(AOuter@geno, 
-#                   center = FALSE, scale = FALSE, scaleG = FALSE,
-#                   nCores = cores, verbose = TRUE) 
-# thetaOuter = thetaOuter / dim(geno(AOuter))[2]
-# gammaOuter = outerGRM / thetaOuter
-# save(gammaOuter, file = "Runs/gammaOuter.Rdata")
-# 
-# thetaOther = getG(AOther@geno, 
-#                   center = FALSE, scale = FALSE, scaleG = FALSE,
-#                   nCores = cores, verbose = TRUE) 
-# thetaOther = thetaOther / dim(geno(AOther))[2]
-# gammaOther = otherGRM / thetaOther
-# save(gammaOther, file = "Runs/gammaMOther.Rdata")
