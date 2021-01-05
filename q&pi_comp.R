f2edf404d22199deb184d3a6e78b05bd153977c2
kennaas <33057681+kennaas@users.noreply.github.com>
2021-01-05 18:29:38 +0100

    Update q&pi_comp.R

--- q&pi_comp.R
+++ q&pi_comp.R
@@ -1,40 +1,30 @@
 ################## Check group membership proportions #############
+
 # How similar is pi to q?
 library(data.table)
 library(BGData)
 library(knitr)
 
+###################### Load data ##################################
+
 load("Runs/morphData_pedigree_version.RData")
 load("Runs/pedigree.RData")
-load.BGData(file = "Data/loter/Run 3/AInner/AInner.RData")
-load.BGData(file = "Data/loter/Run 3/AOuter/AOuter.RData")
-load.BGData(file = "Data/loter/Run 3/AOther/AOther.RData")
+load("Runs/GRMs/GRM_rio3_Inner.RData")
 
-inds = AInner@pheno$ID
 
-z = rep(-1, length(inds))
-pi_mat = data.table(pi_outer = z, pi_inner = z, pi_other = z)
+pi_mat = data.table(ID = rownames(G_VR), pi_inner = pi)
+load("Runs/GRMs/GRM_rio3_Outer.RData")
+pi_mat$pi_outer = pi
+load("Runs/GRMs/GRM_rio3_Other.RData")
+pi_mat$pi_other = pi
 
-num_alleles =  dim(AInner@geno)[2]
+#head(pedigreeData[pedigreeData$ringnr %in% pi_mat$ID, c(1, 10, 11, 12)])
 
-for (ind in 1:length(inds)) {
-  pi_mat$pi_inner[ind] = mean(AInner@geno[ind, ])
-  pi_mat$pi_outer[ind] = mean(AOuter@geno[ind, ])
-  pi_mat$pi_other[ind] = mean(AOther@geno[ind, ])
-  
-  print(paste0("Individual ", as.character(ind), ": \n",
-               as.character(pi_mat[ind, ])))
-}
+#head(pedigreeData[match(pi_mat$ID, pedigreeData$ringnr), c(1, 10, 11, 12)])
+#head(pi_mat)
 
-pi_mat$ID = inds
-
-head(pedigreeData[pedigreeData$ringnr %in% inds, c(1, 8, 9, 10)])
-
-head(pedigreeData[match(inds, pedigreeData$ringnr), c(1, 8, 9, 10)])
-head(pi_mat)
-
-comp = cbind(
-  pedigreeData[match(inds, pedigreeData$ringnr), c(1, 8, 9, 10)], 
-  pi_mat[, c(2, 1, 3)])
+# Create data.frame containing group membership proportions based 
+# on pedigree and genome.
+comp = cbind(pedigreeData[match(pi_mat$ID, pedigreeData$ringnr), c(1, 10:12)], pi_mat[, -1])
 
 save(comp, file = "Runs/q&pi_comp3.RData")
