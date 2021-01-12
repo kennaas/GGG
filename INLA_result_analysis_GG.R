586f64c6cc23309b63c12cea438e547810ec13da
kennaas <33057681+kennaas@users.noreply.github.com>
2021-01-12 10:52:54 +0100

    Changes

--- INLA_result_analysis_GG.R
+++ INLA_result_analysis_GG.R
@@ -8,44 +8,48 @@ if (!require(INLA))
     dep=TRUE)
 library(INLA)
 
-######################## Prior sensitivity analysis ###############
+######################## Options ##################################
+
 response = "wing"
+loter_run = 1
 
-load(paste0("Runs/", response, "/inla_result_hetGG_rio3_VR_", response, ".RData"))
-modelmoderate = model
-load(paste0("Runs/", response, "/inla_result_hetGG_rio3_VR_", response, "_strictprior.RData"))
-modelstrict = model
-load(paste0("Runs/", response, "/inla_result_hetGG_rio3_VR_", response, "_kindprior.RData"))
-modelkind = model
+######################## Prior sensitivity analysis ###############
 
-load(paste0("Runs/", response, "/inla_result_hetGG_pedigree_", response, ".RData"))
-modelpedmoderate = model
-load(paste0("Runs/", response, "/inla_result_hetGG_pedigree_", response, "_strictprior.RData"))
-modelpedstrict = model
-load(paste0("Runs/", response, "/inla_result_hetGG_pedigree_", response, "_kindprior.RData"))
-modelpedkind = model
-
-
-modelmoderate$summary.fixed
-modelkind$summary.fixed
-modelstrict$summary.fixed
-modelmoderate$variance
-modelkind$variance
-modelstrict$variance
-
-modelpedmoderate$summary.fixed
-modelpedkind$summary.fixed
-modelpedstrict$summary.fixed
-modelpedmoderate$variance
-modelpedkind$variance
-modelpedstrict$variance
-
-rm(list = ls())
+#
+# load(paste0("Runs/", response, "/inla_result_hetGG_rio", loter_run, "_VR_", response, ".RData"))
+# modelmoderate = model
+# load(paste0("Runs/", response, "/inla_result_hetGG_rio", loter_run, "_VR_", response, "_strictprior.RData"))
+# modelstrict = model
+# load(paste0("Runs/", response, "/inla_result_hetGG_rio", loter_run, "_VR_", response, "_kindprior.RData"))
+# modelkind = model
+# 
+# load(paste0("Runs/", response, "/inla_result_hetGG_pedigree_", response, ".RData"))
+# modelpedmoderate = model
+# load(paste0("Runs/", response, "/inla_result_hetGG_pedigree_", response, "_strictprior.RData"))
+# modelpedstrict = model
+# load(paste0("Runs/", response, "/inla_result_hetGG_pedigree_", response, "_kindprior.RData"))
+# modelpedkind = model
+# 
+# 
+# modelmoderate$summary.fixed
+# modelkind$summary.fixed
+# modelstrict$summary.fixed
+# modelmoderate$variance
+# modelkind$variance
+# modelstrict$variance
+# 
+# modelpedmoderate$summary.fixed
+# modelpedkind$summary.fixed
+# modelpedstrict$summary.fixed
+# modelpedmoderate$variance
+# modelpedkind$variance
+# modelpedstrict$variance
+# 
+# rm(list = ls())
 
 ######################### Run times ###############################
-response = "wing"
 
-load(paste0("Runs/", response, "/inla_result_hetGG_rio3_VR_", response, ".RData"))
+load(paste0("Runs/", response, "/inla_result_hetGG_rio", loter_run, "_VR_", response, ".RData"))
 summary(model01)
 summary(model02)
 summary(model)
@@ -65,18 +69,20 @@ rm(model02)
 
 ############# Using all genotyped (3) vs. only phenotyped (4) #####
 
-response = "tarsus"
-
-load(paste0("Runs/", response, "/inla_result_hetGG_rio3_VR_", response, ".RData"))
-
-model3 = model
-
-load(paste0("Runs/", response, "/inla_result_hetGG_rio4_VR_", response, ".RData"))
-
-model4 = model
-
-model3$summary.fixed - model4$summary.fixed
-
-model3$variances - model4$variances
-
-# all responses: barely effects results -> safe to use smaller data (faster) data set?
+# response = "tarsus"
+# loter_run = 3
+# 
+# load(paste0("Runs/", response, "/inla_result_hetGG_rio", loter_run, "_VR_", response, ".RData"))
+# 
+# model3 = model
+# loter_run = 4
+# 
+# load(paste0("Runs/", response, "/inla_result_hetGG_rio", loter_run, "_VR_", response, ".RData"))
+# 
+# model4 = model
+# 
+# model3$summary.fixed - model4$summary.fixed
+# 
+# model3$variances - model4$variances
+# 
+# # all responses: barely effects results -> safe to use smaller data (faster) data set?
