232f811658c0e3b830c98238ad7ddd1e67d18f57
kennaas <33057681+kennaas@users.noreply.github.com>
2021-02-08 14:42:14 +0100

    Changed format and improved speed

--- W_setup.R
+++ W_setup.R
@@ -1,77 +1,94 @@
+suppressMessages({
 if (!require(R.utils)) install.packages("R.utils")
 library(R.utils)
 if (!require(data.table)) install.packages("data.table")
 library(data.table)
-
+if (!require(foreach)) install.packages("foreach")
+library(foreach)
+})
+  
 source("My_R_code/file_backed_mat.R")
 
+  
 loter_run = 1
 
-# Load phased haplotype data from different populations
-
-print("Loading data")
-admixedVCF = 
-  fread(file = paste0("Data/loter/Run ", loter_run,
-               "/Loter input ", loter_run,
-               "/admixed_phased", loter_run, ".vcf.gz"),
-        key = "#CHROM,POS,ID,REF,ALT,QUAL,FILTER,INFO,FORMAT")
-innerVCF = 
-  fread(file = paste0("Data/loter/Run ", loter_run,
-               "/Loter input ", loter_run, 
-               "/inner_phased", loter_run, ".vcf.gz"),
-        key = "#CHROM,POS,ID,REF,ALT,QUAL,FILTER,INFO,FORMAT")
-outerVCF = 
-  fread(file = paste0("Data/loter/Run ", loter_run,
-                      "/Loter input ", loter_run,
-                      "/outer_phased", loter_run, ".vcf.gz"),
-        key = "#CHROM,POS,ID,REF,ALT,QUAL,FILTER,INFO,FORMAT")
-otherVCF = 
-  fread(file = paste0("Data/loter/Run ", loter_run,
-                      "/Loter input ", loter_run, 
-                      "/other_phased", loter_run, ".vcf.gz"),
-        key = "#CHROM,POS,ID,REF,ALT,QUAL,FILTER,INFO,FORMAT")
-
-# Merge into one data.table (still on vcf format)
-fullVCF = 
-  merge(innerVCF, merge(outerVCF, merge(otherVCF, admixedVCF)))
-print("Merge complete.")
-
-inds = colnames(fullVCF)[-(1:9)]
-numSNPs = length(fullVCF$ID)
-
-print(numSNPs)
-
-rm(innerVCF)
-rm(outerVCF)
-rm(otherVCF)
-rm(admixedVCF)
-
-# Indices
-SNPs1 = seq(1, by = 2, to = 2 * numSNPs)
-SNPs2 = seq(2, by = 2, to = 2 * numSNPs)
-
-# Create file-backed matrix to hold haplotypes
-W = BGData(pheno = data.frame(ID = inds),
-           geno = initFileBackedMatrix(length(inds),
-                                       2 * numSNPs,
-                                       outputType = "logical",
-                                       folderOut = paste0(
-                                         "Data/loter/Run ", loter_run, "/W")))
-
-# Fill matrix with haplotype data from the VCF format
-for (ind in 10:(9 + length(inds))) {
-  W@geno[ind - 9, SNPs1] = sapply(fullVCF[, ..ind],
-                             function(x) as.numeric(grepl("^1", x)))
-  W@geno[ind - 9, SNPs2] = sapply(fullVCF[, ..ind],
-                             function(x) as.numeric(grepl("1$", x)))
+get_pop = function(group) {
+  groupFile = file(paste0("Data/loter/Run ", loter_run, "/Input/", group, "_phased.vcf.gz"),
+                   open = "r")
+  groupLine = scan(groupFile, nlines = 1, skip = 8,
+                   what = character(), quiet = TRUE)
+  close(groupFile)
+  return(groupLine[-(1:9)])
 }
 
-# Give columns reasonable names
-colnames(W@geno) = do.call("paste0",
-                           c(list(rep(fullVCF$ID, each = 2)),
-                             "_",
-                             list(1:2)))
+# Population is grouped by inner, outer, other, admixed
+innerInds = get_pop("inner")
+outerInds = get_pop("outer")
+otherInds = get_pop("other")
+admixedInds = get_pop("admixed")
+pop = c(innerInds, outerInds, otherInds, admixedInds)
+# Number of inds. in each (sub)population
+numInnerInds = length(innerInds)
+numOuterInds = length(outerInds)
+numOtherInds = length(otherInds)
+numAdmixedInds = length(admixedInds)
+popSize = length(pop)
+
+SNPs = fread(file = paste0("Data/loter/Run ", loter_run, "/Input/admixed_phased.vcf.gz"),
+             select = 3)$ID
+numSNPs = length(SNPs)
+
+# Create file-backed matrix to hold haplotypes.
+# First popSize rows correspond to DNA strand 1 and next popSize correspond to DNA strand 2
+W = BGData(pheno = data.frame(ind = rep(pop, 2), h = rep(c(1, 2), each = popSize)),
+           geno = initFileBackedMatrix(nrows = 2 * popSize, ncols = numSNPs, outputType = "boolean",
+                                       folderOut = paste0("Data/loter/Run ", loter_run, "/W")))
+
+
+indHaplo = function(ind, firstInd, groupVCF) {
+  indVCF = ind + 1 - firstInd
+  W@geno[ind, ] = sapply(groupVCF[indVCF, ], function(x) as.numeric(grepl("^1", x)))
+}
+
+# Fill matrix with haplotype data from the VCF format
+fillW = function(group, firstInd, lastInd) {
+  print("Loading haplotypes...")
+  groupVCF = fread(file = paste0("Data/loter/Run ", loter_run, "/Input/", group, "_phased.vcf.gz"),
+                   drop = 1:9)
+  print("Parsing haplotypes...")
+  h1 = lapply(groupVCF, function(x) grepl("^1", x))
+  h2 = lapply(groupVCF, function(x) grepl("1$", x))
+
+  rm(groupVCF)
+  
+  print("Saving haplotypes...")
+  for (ind in firstInd:lastInd) {
+    iVCF = ind + 1 - firstInd
+    W@geno[ind, ] = h1[[iVCF]]
+    W@geno[ind + popSize, ] = h2[[iVCF]]
+    # print(paste0("Individual ", ind))
+  }
+  rm(h1, h2)
+  
+  # mclapply(firstInd:lastInd, indHaplo, firstInd, groupVCF, mc.cores = numCores)
+  # rm(groupVCF)
+  print("Done.")
+}
+
+print("Parsing inner")
+fillW("inner", 1, numInnerInds)
+
+print("Parsing outer")
+fillW("outer", numInnerInds + 1, numInnerInds + numOuterInds)
+
+print("Parsing other")
+fillW("other", numInnerInds + numOuterInds + 1, popSize - numAdmixedInds)
+
+print("Parsing admixed")
+fillW("admixed", numInnerInds + numOuterInds + numOtherInds + 1, popSize)
+
+rownames(W@map) = colnames(W@geno) = SNPs
+rownames(W@pheno) = rownames(W@pheno) = apply(W@pheno, 1, paste, collapse = "_")
 
 # Save
 save(W, file = paste0("Data/loter/Run ", loter_run, "/W/W.RData"))
-
